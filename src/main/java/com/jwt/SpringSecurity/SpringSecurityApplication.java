84a8c9071da7e02fff459fecf900c93a67fa322b
Vishva Balar <vishubalar29@gmail.com>
2025-03-26 17:51:25 +0530

    First Commit

---
+++ src/main/java/com/jwt/SpringSecurity/SpringSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.jwt.SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringSecurityApplication.class, args);
+		System.out.println("hello i m running...");
+	}
+
+}
