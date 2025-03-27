aaba0c6d742cfe334d2d785d1e4cf624ac33d71c
Vishva Balar <vishubalar29@gmail.com>
2025-03-27 19:04:22 +0530

    CSRF Token done

---
+++ src/main/java/com/jwt/SpringSecurity/StudentController/StudentController.java
@@ -0,0 +1,36 @@
+package com.jwt.SpringSecurity.StudentController;
+
+import com.jwt.SpringSecurity.StudentModel.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Vishva",98),
+            new Student(2, "Rahi", 89),
+            new Student(3, "Prisha", 91)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student){
+        students.add(student);
+        return student;
+    }
+
+}
